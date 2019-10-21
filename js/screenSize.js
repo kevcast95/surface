00507fc7c342af4c6f696c29f4bb15ce55af7568
Kevin Cast <kevcast06@gmail.com>
2019-10-21 01:12:02 -0500

    advance for tablet

---
+++ js/screenSize.js
@@ -0,0 +1,8 @@
+window.onload =function screenSize(){
+    var w = window.innerWidth;
+    console.log('width: ', w);
+    if(w >= 768){
+        header = document.getElementById('header')
+      
+    }
+}
